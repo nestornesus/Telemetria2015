f7239a81108199920b8519a89f26cab51694a43e
Athesto <gamez.live@gmail.com>
2015-03-21 22:05:18 -0500

    Limpieza de Codigo
    Se borran los archivos innecesarios y se incluye la sintaxys de los
    eventos

--- ConfiguraciónBaseDeDatosPy/ExecuteSnifferMySQL.py
+++ ConfiguraciónBaseDeDatosPy/ExecuteSnifferMySQL.py
@@ -5,3 +5,9 @@ print ""
 import MySQL.CapturaDeDatos
 
 raw_input("Press Enter to continue...")
+
+#Syntaxis del evento que se envia 
+#data   = >REV 37 1835 6 00502 +10_98894 -074_81161 000 287 3 2;ID=GRUPO2<
+#Syntax = CCCC II WWWW D TTTTT XXX xxxxx YYYY yyyyy SSS HHH M A[EXTENDED-EV TAGS]
+#                      1            2           3             4
+#datapos= 0123 45 6789 0 12345 678 90123 4567 89012 345 678 9 0
